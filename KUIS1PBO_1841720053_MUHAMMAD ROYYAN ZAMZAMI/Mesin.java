b32dccc96e460c25203c3327bad322a94d32f349
muhammadroyyan11 <royyanmz87@gmail.com>
2019-10-04 15:16:31 +0700

    upload

---
+++ KUIS1PBO_1841720053_MUHAMMAD ROYYAN ZAMZAMI/Mesin.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Quis1;
+
+/**
+ *
+ * @author Royyan
+ */
+public class Mesin {
+    public int speed;
+    public int gear;
+    public int speedMax;
+    public int speedMin;
+    public int speedAwal;
+    public int speedDown;
+
+    public Mesin() {
+    }
+
+    public Mesin(int speed, int gear, int speedMax, int speedMin) {
+        this.speed = speed;
+        this.gear = gear;
+        this.speedMax = speedMax;
+        this.speedMin = speedMin;
+        
+    }
+
+    public Mesin(int speedAwal) {
+        this.speedAwal = speedAwal;
+    }
+    
+    public int getSpeedAwal() {
+        return speedAwal;
+    }
+
+    public void setSpeedAwal(int speedAwal) {
+        this.speedAwal = speedAwal;
+    }
+
+    public int getSpeedDown() {
+        return speedDown;
+    }
+
+    public void setSpeedDown(int speedDown) {
+        this.speedDown = speedDown;
+    }
+    
+    
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getGear() {
+        return gear;
+    }
+
+    public void setGear(int gear) {
+        this.gear = gear;
+    }
+
+    public int getSpeedMax() {
+        return speedMax;
+    }
+
+    public void setSpeedMax(int speedMax) {
+        this.speedMax = speedMax;
+    }
+
+    public int getSpeedMin() {
+        return speedMin;
+    }
+
+    public void setSpeedMin(int speedMin) {
+        this.speedMin = speedMin;
+    }
+    
+    public void gantiGear(int newValue){
+        gear = newValue;
+    }
+    public void tambahKecepatan(int increment){
+        speed = speedAwal + increment;
+    }
+    public void rem(int decrement){
+        speed = speedAwal - decrement;
+    }
+    
+    public void cetakStatus(){
+        System.out.println("Kecepatan Maksimal : " + speedMax + " km/jam");
+        System.out.println("Kecepatan Minimal : " + speedMin + " km/jam");
+        System.out.println("Masukkan gear : " +gear);
+        System.out.println("Kecepatan Awal : " +speedAwal);
+        System.out.println("Tambah kecepatan : " + speed);
+        System.out.println("Kurangi Kecepatan : " + speedDown);
+    }
+}
---
+++ KUIS1PBO_1841720053_MUHAMMAD ROYYAN ZAMZAMI/Mobil.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Quis1;
+import java.util.Scanner;
+/**
+ *
+ * @author Royyan
+ */
+public class Mobil extends Mesin{
+    public String merk;
+
+    Scanner sc = new Scanner(System.in);
+    
+    public Mobil(String merk, int speed, int gear, int speedMax, int speedMin) {
+        super(speed, gear, speedMax, speedMin);
+        this.merk = merk;
+    }
+
+    public Mobil() {
+        
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getGear() {
+        return gear;
+    }
+
+    public void setGear(int gear) {
+        this.gear = gear;
+    }
+
+    public int getSpeedMax() {
+        return speedMax;
+    }
+
+    public void setSpeedMax(int speedMax) {
+        this.speedMax = speedMax;
+    }
+
+    public int getSpeedMin() {
+        return speedMin;
+    }
+
+    public void setSpeedMin(int speedMin) {
+        this.speedMin = speedMin;
+    }
+
+    public void cetakData(){
+        int maneh = 1;
+        while (maneh == 1){
+        System.out.println("Merk Mobil \n1. Ferrari \n2. Renault \n3.Mercedes");
+        System.out.println("");
+        System.out.print("Pilih Merk Mobil Anda : ");
+        int pilih = sc.nextInt();
+        System.out.println("");
+        if (pilih==1) {
+            System.out.println("Merk Mobil : Ferrari");
+            speedMax = 430;
+            speedMin = 0;
+            gear = 1;
+            speedAwal = 200;
+            speed = speedAwal + 30;
+            speedDown = speedAwal - 10;
+            super.getSpeed();
+            super.cetakStatus();
+        }
+        else if (pilih == 2) {
+            System.out.println("Merk Mobil : Renault");
+            speedMax = 385;
+            speedMin = 0;
+            gear = 1;
+            speedAwal = 100;
+            speed = speedAwal + 55;
+            speedDown = speedAwal - 20;
+            super.cetakStatus();
+        }
+        
+        else if (pilih == 3) {
+            System.out.println("Merk Mobil : Mercedes");
+            speedMax = 430;
+            speedMin = 0;
+            gear = 1;
+            speedAwal = 100;
+            speed = speedAwal + 65;
+            speedDown = speedAwal - 35;
+            super.cetakStatus();
+        }
+        else{
+            System.out.println("Pilihan Tidak Tersedia");
+        }
+        System.out.print("Apakah Anda Ingin Kredit Lagi Y/T?");
+	
+        char ya = sc.next().charAt(0);
+	// ya = sc.next().charAt(0);
+
+	if (ya=='Y' || ya == 'y') {
+            maneh = 1;
+	}
+	else if (ya=='T' || ya == 't'){
+            maneh = 0;
+	}
+        System.out.println("");
+        }
+    }
+}
