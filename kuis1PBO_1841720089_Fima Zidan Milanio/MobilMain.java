2194a373fba2d2c10e145a57c35db28896cddc13
rezaLUL <rezaulull@gmail.com>
2019-10-04 16:15:55 +0700

    upload

---
+++ kuis1PBO_1841720089_Fima Zidan Milanio/MobilMain.java
@@ -0,0 +1,20 @@
+
+import Kuis1.Mobil;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class MobilMain {
+    public static void main(String[] args) {
+        Mobil m = new Mobil();
+        
+        m.cetakData();
+    }
+}
